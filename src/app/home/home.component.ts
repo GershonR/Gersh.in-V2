03abf1a7280a9aaf073542a043185f303c0200ec
Gershon <reydmang@myumanitoba.ca>
2018-02-16 22:41:19 -0600

    Video background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,13 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: '',
-  styles: [`
-  `],
+  styleUrls: ['./home.css']
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
-  public localState: any;
-  constructor(
-    public route: ActivatedRoute
-  ) {}
-
-  public ngOnInit() {
-    this.route
-      .data
-      .subscribe((data: any) => {
-        this.localState = data.yourData;
-      });
-
-    console.log('hello `Home` component');
-  }
 
 
 }
