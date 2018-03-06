4a62d3e526dc54d706d3ef4db47e2219b9dab0e4
Gershon <reydmang@myumanitoba.ca>
2018-03-05 20:22:58 -0600

    Development Skills on Home Page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
+declare var jQuery:any;
 @Component({
   selector: '',
   styleUrls: ['./home.css'],
   templateUrl: './home.component.html'
 })
-export class HomeComponent  {
+export class HomeComponent implements AfterViewInit {
 	
 	public muted = true;
 	
 	toggleMute() {
 		console.log(this.muted);
 		this.muted = !this.muted;
+	}
+	
+	ngAfterViewInit(): void {
+		jQuery('h1').css({'opacity':( 100-jQuery(window).scrollTop() )/100});
+		
+		jQuery(window).scroll(function(){
+			alert("Called");
+			jQuery(".top").css("opacity", 1 - jQuery(window).scrollTop() / 250);
+		});
 	}
 
 }
